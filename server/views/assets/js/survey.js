d2a2d03e42a843745a0159d262dd1e00a3a2dcd7
Snoodleking <duncaniaria@gmail.com>
2017-08-06 19:47:39 -0400

    questions buttons now response to clicks

--- server/views/assets/js/survey.js
+++ server/views/assets/js/survey.js
@@ -3,24 +3,50 @@
 //=========================
 import '../scss/survey.scss';
 
-function test( tEvent )
+//survey controller module declaration
+var surveyController = ( function()
 {
-    console.log( tEvent );
-}
+    const publicAPI = 
+    {
+        init: init,
+    }
 
-const buttons = document.querySelectorAll( '.question-button' );
+    return publicAPI;
 
-buttons.forEach( ( button ) => {
-    button.addEventListener( 'click', onClicked );
-}); 
+    //=========================
+    //  INIT
+    //=========================
+    function init()
+    {
+        initSurveyButtons();
+    }
+    
+    //can be used to attatch any handler to any elementt
+    function attachClickHandler( tButton, tHandler )
+    {
+        tButton.addEventListener( 'click', tHandler );
+    }
 
-function onClicked( tEvent )
-{
-    console.log('clicked');
-    console.log( tEvent );
-    console.log( tEvent.target.dataset.value );
-    console.log( tEvent.target.parentElement.dataset.id );
-       
-}
+    //setup survey value buttons
+    function initSurveyButtons()
+    {
+        //get all buttons
+        const buttons = document.querySelectorAll( '.question-button' );
+
+        //attatch button to all survey questions
+        buttons.forEach( function( button ){ attachClickHandler( button, onSurveyValueClicked ) } );
+    }
+
+    //survey value button functionality
+    function onSurveyValueClicked( tEvent )
+    {
+        console.log(' clicked' );
+        console.log( tEvent );
+        console.log( tEvent.target.dataset.value );
+        console.log( tEvent.target.parentElement.dataset.id );
+    }
+})();
+
+surveyController.init();
 
 console.log( 'survey js loaded' );
