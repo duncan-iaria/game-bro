cb30c7588f79f414f08287f1bc60e865cabdd86c
Snoodleking <duncaniaria@gmail.com>
2017-08-06 16:12:06 -0400

    added temp survey questions and script

--- server/views/assets/js/main.js
+++ server/views/assets/js/main.js
@@ -4,3 +4,10 @@
 import '../scss/style.scss';
 
 console.log( "hey i'm here" );
+
+function clickMang()
+{
+    console.log('click mang');
+}
+
+const testVar = "hey";
--- server/views/assets/js/survey.js
+++ server/views/assets/js/survey.js
@@ -3,4 +3,24 @@
 //=========================
 import '../scss/survey.scss';
 
+function test( tEvent )
+{
+    console.log( tEvent );
+}
+
+const buttons = document.querySelectorAll( '.question-button' );
+
+buttons.forEach( ( button ) => {
+    button.addEventListener( 'click', onClicked );
+}); 
+
+function onClicked( tEvent )
+{
+    console.log('clicked');
+    console.log( tEvent );
+    console.log( tEvent.target.dataset.value );
+    console.log( tEvent.target.parentElement.dataset.id );
+       
+}
+
 console.log( 'survey js loaded' );
